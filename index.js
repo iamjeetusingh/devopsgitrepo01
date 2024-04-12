fec6cba2386fdbcc44684682b01f71e691501f02
Jitendra Singh Tomar <jitendrastomar5593@gmail.com>
2024-04-12 21:27:53 +0530

    Update index.js 3

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
   res.write('You are  in AWS  Cloud. THis is Jeetu, hahah'); 
-  res.end(); //end the response
+  res.end();     //end the response
 }).listen(4000); //the server object listens on port 4000
